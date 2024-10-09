73f704be526295b8d99306bf2cd31f7a2484293f
Yash Pandey <yashdevesh10@gmail.com>
2024-10-09 20:23:19 +0530

    port binding

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 // const PORT = 8082;
 // const Mongo_URL = "mongodb://127.0.0.1:27017/task-manager";
 const PORT = process.env.PORT  || 10000;
-const Mongo_URL = process.env.MongoDB_URL;
+const Mongo_URL = process.env.MONGODB_URL;
 
 mongoose.connect(Mongo_URL
     // ,{useNewUrlParser : true,
